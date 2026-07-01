2cf255027b3d7100bd17acef1ca891878fa96ea3
willy9015 <215429032+willy9015@users.noreply.github.com>
2026-07-01 20:07:16 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
 		outDir: 'dist',
 		emptyOutDir: true,
 		sourcemap: false,
-		minify: 'terser',
+		minify: 'esbuild',
 		rollupOptions: {
 			input: {
 				// ── Auth / onboarding ──────────────────────────
